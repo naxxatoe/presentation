1dfcc498de15aee2f9e2bb62b1a43f2915c94630
Martin Ptacek <natano@nicenamecrew.com>
2009-09-12 02:42:45 +0200

    deleted license output at programm startup

--- pysrc/DouF00/douf00.py
+++ pysrc/DouF00/douf00.py
@@ -80,10 +80,6 @@ def filetype(path):
 class MyApp(wx.App):
     def OnInit(self):
         print(cfg.title + ' ' + cfg.__version__)
-        print('Copyright (C) 2009  ' + cfg.__author__)
-        print('This program comes with ABSOLUTELY NO WARRANTY.')
-        print('This is free software, and you are welcome to redistribute it')
-        print('and/or modify it under the terms of the MIT license.')
 
         atexit.register(die)
         try:
